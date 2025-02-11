179dd752f1277d7ced0212152de8195534f09c10
atat1000 <126907963+atat1000@users.noreply.github.com>
2025-02-11 14:41:18 -0800

    Add files via upload

---
+++ js_demo/js/script.js
@@ -0,0 +1,8 @@
+//alert('this is sigma')
+
+function askQuestion() {
+    var p = prompt('how much do you spend on minecraft?');
+    if (p != null) {
+        document.getElementById('question').innerHTML = 'Yikes! ' + p + ' that is a lot of time'
+    }    I
+}
